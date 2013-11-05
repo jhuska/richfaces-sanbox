4eede9ffec70d65746e333993f12547d954d5835
Juraj Huska <jhuska@redhat.com>
2013-11-05 09:28:40 +0100

    234055

---
+++ java/org/richfaces/demo/common/RichBean.java
@@ -0,0 +1,87 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.richfaces.demo.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class RichBean {
+    
+    private String host;
+    private int port;
+    private List<String> suggestions;
+    private String docServer;
+    
+    @PostConstruct
+    public void initialiseSuggestions() { 
+        setSuggestions(new ArrayList<String>());
+        getSuggestions().add("Foo");
+        getSuggestions().add("Bar");
+        getSuggestions().add("Blah");
+        getSuggestions().add("Yeah");
+        getSuggestions().add("Underground");
+        getSuggestions().add("Still");
+    }
+    
+    public void actionListener() {
+        System.out.println("Action Listener called on the server!");
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getDocServer() {
+        return docServer;
+    }
+
+    public void setDocServer(String docValue) {
+        this.docServer = docValue;
+    }
+
+    public List<String> getSuggestions() {
+        return suggestions;
+    }
+
+    public void setSuggestions(List<String> suggestions) {
+        this.suggestions = suggestions;
+    }
+}
